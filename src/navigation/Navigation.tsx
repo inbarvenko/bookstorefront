b67cd6e0985f8c7ea306038113b3effb36b1fc70
inbarvenko <Inga_barvenko@mail.ru>
2023-08-30 11:22:54 +0300

    added bootsplash

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import 'react-native-gesture-handler';
 import TabNavigation from './TabNavigation';
+import BootSplash from 'react-native-bootsplash';
 
 const RootStackComponent: React.FC = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      onReady={() => {
+        BootSplash.hide();
+      }}>
       <TabNavigation />
     </NavigationContainer>
   );
